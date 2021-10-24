0cae000f91e4323345ced97fb4a57868a27234a2
Alexander Chesser <alex.chesser@gmail.com>
2021-10-23 23:52:04 -0400

    updated html display with QOL features

--- Writers/JobsTableWriter.cs
+++ Writers/JobsTableWriter.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Text.RegularExpressions;
 
 namespace netflix_jobs_api_scraper
 {
@@ -13,22 +14,25 @@ namespace netflix_jobs_api_scraper
         public async Task WriteAsync(TextWriter tw, string[] header, List<string[]> postings)
         {
             string joiner = "</td><td>";
-            await tw.WriteLineAsync(@"<style>td{vertical-align: top;}tr:nth-child(even) {background-color: #f2f2f2;}</style>");
-            await tw.WriteLineAsync("<table>");
+            await tw.WriteLineAsync(await File.ReadAllTextAsync("./Resources/tablehead.html"));
+            await tw.WriteLineAsync("<table id='demo'>");
             await tw.WriteLineAsync("<thead><tr>");
             await tw.WriteAsync("<td>");
             await tw.WriteAsync(string.Join(joiner, header));
             await tw.WriteLineAsync("<td>");
             await tw.WriteLineAsync("</tr></thead>");
             await tw.WriteLineAsync("<tbody>");
+            Regex rxp = new Regex(@">(\d+)<\/a>", RegexOptions.Compiled);
             foreach (var posting in postings)
             {
-                await tw.WriteAsync("<tr><td>");
+                await tw.WriteAsync($"<tr><td>");
+                posting[posting.Length - 1] += $"<input type='checkbox' class='yes' value='{rxp.Match(posting[0]).Groups[1].Value}'>";
                 await tw.WriteAsync(string.Join(joiner, posting));
                 await tw.WriteLineAsync("</td></tr>");
             }
             await tw.WriteLineAsync("</tbody>");
             await tw.WriteLineAsync("</table>");
+            await tw.WriteLineAsync(await File.ReadAllTextAsync("./Resources/tablefoot.html"));
         }
     }
 }
