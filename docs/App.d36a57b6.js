15195147a03835611b92c580d3a6e39f39ee3707
Alexander Chesser <alex.chesser@gmail.com>
2021-10-31 21:29:54 -0400

    gross hack - fix better later

--- docs/App.d36a57b6.js
+++ docs/App.d36a57b6.js
@@ -35083,7 +35083,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52391" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60844" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
